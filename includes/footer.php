8215793cd8bd3b15566b87e50cd04a814314d13d
Esmondsmith <133409203+Esmondsmith@users.noreply.github.com>
2023-12-05 09:43:40 +0100

    Using a datepicker function

--- includes/footer.php
+++ includes/footer.php
@@ -10,6 +10,7 @@
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 
     <script>
+    //Here, i used a datepicker function from jqueryui.com
        $( function() {
             $( "#dob" ).datepicker({
             changeMonth: true,
